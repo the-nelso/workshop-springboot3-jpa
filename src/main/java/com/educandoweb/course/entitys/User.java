82d1def8ae690fbbf595880ea5534115a82cd999
Nelson Meduna <nelsonmeduna34@gmail.com>
2024-01-31 16:12:16 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/educandoweb/course/entitys/User.java
+++ src/main/java/com/educandoweb/course/entitys/User.java
@@ -1,12 +1,17 @@
 package com.educandoweb.course.entitys;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -22,15 +27,19 @@ public class User implements Serializable{
 	private String email;
 	private String password;
 	
+	@JsonIgnore
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+	
 	public User() {
 	}
 	
-	public User(Long id, String name, String phone, String email, String password) {
+	public User(Long id, String name, String email, String phone, String password) {
 		super();
 		this.id = id;
 		this.name = name;
-		this.phone = phone;
 		this.email = email;
+		this.phone = phone;
 		this.password = password;
 	}
 
@@ -65,6 +74,18 @@ public class User implements Serializable{
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public List<Order> getOrders() {
+		return orders;
+	}
 
 	@Override
 	public String toString() {
@@ -86,13 +107,5 @@ public class User implements Serializable{
 			return false;
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
 	}
 }
---
+++ src/main/java/com/educandoweb/course/services/OrderService.java
@@ -0,0 +1,29 @@
+package com.educandoweb.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.course.entitys.Order;
+import com.educandoweb.course.entitys.User;
+import com.educandoweb.course.repositories.OrderRepository;
+import com.educandoweb.course.repositories.UserRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository orderRepository;
+	
+	public List<Order> findAll(){
+		return orderRepository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj = orderRepository.findById(id);
+		
+		return obj.get();
+	}
+}
