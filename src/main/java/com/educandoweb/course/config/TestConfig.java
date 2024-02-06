6cf7c3a476a6bafce4a27ee6c060e5c3a16af9af
Nelson Meduna <nelsonmeduna34@gmail.com>
2024-02-05 21:42:21 -0300

    Payment, one-to-one association

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.educandoweb.course.entitys.Category;
 import com.educandoweb.course.entitys.Order;
 import com.educandoweb.course.entitys.OrderItem;
+import com.educandoweb.course.entitys.Payment;
 import com.educandoweb.course.entitys.Product;
 import com.educandoweb.course.entitys.User;
 import com.educandoweb.course.entitys.enums.OrderStatus;
@@ -79,5 +80,10 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
